6887e83194ae780ba24d648c62af627ac64f5094
Erucix <107660785+erucix@users.noreply.github.com>
2023-11-06 13:34:12 +0530

    Update kiit.js

--- kiit.js
+++ kiit.js
@@ -1,5 +1,5 @@
-const https = require("https");
-const { argv, exit } = require("process");
+import https from "https";
+import { argv } from "process";
 
 let requestOptions = {
     "headers": {
@@ -10,73 +10,151 @@ let requestOptions = {
     "method": "POST"
 };
 
-const requestURI = "https://form.kiit.ac.in/payment/";
+const URI = {
+    "GETID": "https://form.kiit.ac.in/payment/",
+    "GETDETAILS": "https://form.kiit.ac.in/payment/app.php?id="
+}
 
+const UTILITIES = {
+    /* Fetches user ID used to identify each transaction
+     *
+     *
+     * @param (number) rollNumber - Roll Number to search for.
+     * @returns (string) - User ID associated for each request.
+    */
 
-async function getID(rollNumber) {
-    return new Promise((resolve) => {
+    "getID": async function (rollNumber) {
 
-        try {
-            let request = https.request(requestURI, requestOptions, (response) => {
-                if (!response.headers["location"]) {
+        return new Promise((resolve) => {
+
+            let response = https.request(URI.GETID, requestOptions, (answer) => {
+
+                // User ID is in response headers.
+
+                let responseId = answer.headers.location;
+
+                /*
+                 * If user is not found the status code is 200
+                 * If user is found the status code is 302
+                 */
+
+                if (answer.statusCode != 302 || responseId == undefined) {
                     console.log("[\x1b[1;35m!\x1b[0m] Roll number not found.");
-                    exit(0);
-                }
-                resolve(response.headers["location"]);
-            });
-
-            request.write(`appno=${rollNumber}`);
-            request.end();
-        } catch {
-            throw "Some error occured. Try checking your internet connection"
-        }
-
-    })
-}
-
-async function getDetails(id) {
-    return new Promise((resolve) => {
-        let request = https.request(requestURI + id, requestOptions, (response) => {
-            let htmlResponse = "";
-            response.on("data", (chunk) => {
-                htmlResponse += chunk;
-            });
-
-            response.on("end", () => {
-                const values = {};
-
-                let match;
-                let pattern = /<input type="hidden" name="([^"]+)" value="([^"]+)">/g;
-
-                while ((match = pattern.exec(htmlResponse)) !== null) {
-                    const name = match[1];
-                    const value = match[2];
-                    values[name] = value;
+                    process.exit(0);
                 }
 
-                if (values.name == undefined) throw "Error"
+                /*
+                 * The "location" key which contains User ID
+                 * is in the form "app.php?id=3g1QFa364Z" so
+                 * removing everything before "="
+                 */
 
-                resolve(values);
+                resolve(responseId.slice(responseId.indexOf("=") + 1));
             });
-        });
 
-        request.write("amount=1");
-        request.end();
-    })
+            /*
+             * One thing here to note is primitive ways of
+             * fetching or requesting like we are fetching
+             * above using "https.request", the body part
+             * for POST request is supplied after  using
+             * ".write()" function.
+             */
+
+            response.write("appno=" + rollNumber);
+
+            /*
+             * ".end()" function ends the write stream and
+             * also and actually sends the request to server
+             */
+
+            response.end();
+        })
+    },
+    /* Fetches details using specified userID.
+     *
+     *
+     * @param (string) - userID - UserID for each request
+     * @returns (object) - User details as object
+    */
+
+
+    "getDetails": async function (userID) {
+        return new Promise((resolve) => {
+
+            let response = https.request(URI.GETDETAILS + userID, requestOptions, (answer) => {
+
+                /*
+                 * Some issues exist with KIIT payment page
+                 * and sometimes responds with status code
+                 * different from 200
+                 */
+
+                let content = "";
+
+                if (answer.statusCode != 200) {
+                    console.log("[\x1b[1;35m!\x1b[0m] Ugh! Resposne code was not expected.");
+                    process.exit(0);
+                }
+
+                /*
+                 * "data" event is called everytime chunk is
+                 * recieved like easy_curl_init() in C.
+                 */
+                answer.on("data", (chunk) => {
+                    content += chunk;
+                });
+
+                answer.on("end", () => {
+
+                    /* This regex defines pattern for name & values
+                     * at once and "pattern.exec" will be executed
+                     * until the searches matches the pattern.
+                     */
+
+                    let pattern = /name="([^"]+)"\s+value="([^"]+)"/g;
+                    let match;
+                    let result = {};
+
+                    while ((match = pattern.exec(content)) != null) {
+                        result[match[1]] = match[2];
+                    }
+
+                    resolve(result);
+                })
+
+            });
+
+            response.write("amount=1");
+
+            /*
+             * ".end()" function ends the write stream and
+             * also and actually sends the request to server
+             */
+
+            response.end();
+        })
+    }
 }
 
-if (argv[2]) {
-    console.log("[\x1b[1;33m~\x1b[0m] Searching student.");
-    getID(argv[2]).then(data => {
-        console.log("[\x1b[1;33m~\x1b[0m] Digging details.")
-        getDetails(data).then(data => {
-            console.log("\n[\x1b[1;32m*\x1b[0m] \x1b[1;34mName\x1b[0m: ", data.name);
-            console.log("[\x1b[1;32m*\x1b[0m] \x1b[1;34mE-Mail\x1b[0m: ", data.email);
-            console.log("[\x1b[1;32m*\x1b[0m] \x1b[1;34mRoll No\x1b[0m: ", data.roll_no);
-            console.log("[\x1b[1;32m*\x1b[0m] \x1b[1;34mStudent ID\x1b[0m: ", data.student_id);
-        }).catch(err => console.log(err));
-    }).catch(err => console.log(err));;
-} else {
-    console.log("[!] Please specify roll number.");
-    console.log("Eg: node kiit.js 230XXXXX");
+
+if (!argv[2]) {
+    console.log("[\x1b[1;35m!\x1b[0m] Please specify roll number.");
+    console.log("Usage: node kiit.js 230XXXXX");
+    process.exit(1);
 }
+
+console.log("[\x1b[1;33m~\x1b[0m] Fetching User ID...");
+
+let ID = await UTILITIES.getID(argv[2]);
+
+console.log("[\x1b[1;33m~\x1b[0m] Fetching User Details...\b");
+
+let result = await UTILITIES.getDetails(ID);
+
+console.log("");
+
+console.log("[\x1b[1;32m*\x1b[0m] \x1b[1;34mName       \x1b[0m:", result.name.replace("  ", " "));
+console.log("[\x1b[1;32m*\x1b[0m] \x1b[1;34mPhone      \x1b[0m:", result.phone);
+console.log("[\x1b[1;32m*\x1b[0m] \x1b[1;34mE-Mail     \x1b[0m:", result.email);
+console.log("[\x1b[1;32m*\x1b[0m] \x1b[1;34mRoll Number\x1b[0m:", result.roll_no);
+console.log("[\x1b[1;32m*\x1b[0m] \x1b[1;34mStudent ID \x1b[0m:", result.student_id);
